2ac46619b4a94d6bf51bee306c5b46b3eac0d73d
imon_bayazid <imon_bayazid@yahoo.com>
2022-02-16 11:15:33 +0100

    added logger methods

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,15 +1,34 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 	
-	@RequestMapping("/test")
-	public String test()
-	{
-	  return "i am from a new test";	
+	Logger log = org.slf4j.LoggerFactory.getLogger(TestprojectApplication.class);
+
+	@GetMapping("/test")
+	public String test() {
+		log.info("test log");
+		return "this is a test...";
 	}
+	
+	
+	@GetMapping("/testmyerror")
+	public String testmyerror() {
+		log.error("test testmyerror");
+		return "this is a testmyerror";
+	}
+	
+	@GetMapping("/test2")
+	public String test2() {
+		log.info("test error test2");
+		return "this is a test2....";
+	}
+	
+	
 
 }
