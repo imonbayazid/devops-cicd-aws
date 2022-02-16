f7997cebcd6aa186603fe7ddfab54033d0531015
imon_bayazid <imon_bayazid@yahoo.com>
2022-02-16 10:50:02 +0100

    added logging

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,3 @@
-FROM tomcat:8.0.20-jre8
-
-COPY target/demo*.war /usr/local/tomcat/webapps/demo.war
-
-#EXPOSE 8080
-
-#CMD ["catalina.sh", "run"]
+From tomcat:8.0.51-jre8-alpine
+COPY ./target/demo-0.0.1-SNAPSHOT.war /usr/local/tomcat/webapps/demos.war
+CMD ["catalina.sh","run"]
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,29 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	
+	Logger log = org.slf4j.LoggerFactory.getLogger(DemoApplication.class);
 
 	@GetMapping("/test")
 	public String test() {
+		log.info("test log");
 		return "this is a test";
+	}
+	
+	@GetMapping("/error")
+	public String error() {
+		log.error("test error");
+		return "this is a error";
 	}
 	
 	public static void main(String[] args) {
